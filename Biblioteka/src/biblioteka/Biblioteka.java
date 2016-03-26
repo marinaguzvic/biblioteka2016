26896a79dec1a25c10312471fc637f269ee0bafe
Marina Guzvic <marina.guzvic@gmail.com>
2016-03-26 14:16:17 +0100

    Dodata logicka kontrola u klasi Biblioteka

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -9,11 +9,13 @@ public class Biblioteka implements BibliotekaInterfejs {
 
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
+		if(knjiga == null || knjige.contains(knjiga)) throw new RuntimeException("Greska pri unosu knjige!"); 
 		knjige.add(knjiga);
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga knjiga) {
+		if(knjiga == null || knjige.contains(knjiga)) throw new RuntimeException("Greska pri brisanju knjige!"); 
 		knjige.remove(knjiga);
 	}
 
@@ -24,7 +26,11 @@ public class Biblioteka implements BibliotekaInterfejs {
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, long ISBN, String naslov, String izdavac) {
+		if (autor==null && ISBN==0 && naslov==null && izdavac==null) 
+			throw new RuntimeException("Morate uneti bar neki kriterijum za pretragu");
+		
 		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
+		
 		for (Knjiga knjiga : knjige) {
 			if(knjiga.getNaslov().contains(naslov)) rezultat.add(knjiga);
 		}
